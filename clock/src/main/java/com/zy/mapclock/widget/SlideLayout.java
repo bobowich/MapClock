c4a787062beb9d3b875ed45f0504ff3336de044e
bobowich <www.1152587482@qq.com>
2016-09-16 10:05:49 +0800

    finally it works

--- clock/src/main/java/com/zy/mapclock/widget/SlideLayout.java
+++ clock/src/main/java/com/zy/mapclock/widget/SlideLayout.java
@@ -1,12 +1,11 @@
 package com.zy.mapclock.widget;
 
 import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,35 +19,21 @@ public class SlideLayout extends ViewGroup {
     View menuLayout;
     View contentLayout;
     private ViewDragHelper mDragHelper;
-    float mOffset = 0;
-    GestureDetector mGestureDetector;
+    float mOffset ;
+    //GestureDetector mGestureDetector;
     public SlideLayout(Context context) {
-        super(context,null);
+        this(context,null);
     }
 
     public SlideLayout(Context context, AttributeSet attrs) {
-        super(context, attrs,0);
-    }
-
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            if (Math.abs(distanceX) > Math.abs(distanceY)) {
-                return true;
-            }
-            return false;
-        }
-    }
-
-    public SlideLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-
-        super(context, attrs, defStyleAttr);
+        super(context, attrs);
         Log.d(TAG, "SlideLayout: constractor");
-        mGestureDetector = new GestureDetector(getContext(), new MyGestureListener());
+        //mGestureDetector = new GestureDetector(getContext(), new MyGestureListener());
 
         mDragHelper = ViewDragHelper.create(this, 1.0f, new ViewDragHelper.Callback() {
             @Override
             public boolean tryCaptureView(View child, int pointerId) {
+                Log.d(TAG, "tryCaptureView: true");
                 return child == menuLayout;
             }
 
@@ -89,19 +74,32 @@ public class SlideLayout extends ViewGroup {
                 //super.onViewPositionChanged(changedView, left, top, dx, dy);
                 int width = changedView.getMeasuredWidth();
                 float offset = (width + changedView.getLeft()) * 1.0f / width;
+                Log.d(TAG, "onViewPositionChanged: " + offset);
                 mOffset = offset;
                 changedView.setVisibility(offset == 0 ? INVISIBLE : VISIBLE);
-                invalidate();
+                requestLayout();
             }
-
+            /*
             @Override
             public int getViewHorizontalDragRange(View child) {
                 return child == menuLayout ? menuLayout.getWidth() : 0;
             }
+            */
         });
         mDragHelper.setEdgeTrackingEnabled(ViewDragHelper.EDGE_LEFT);
-
     }
+    /*
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            if (Math.abs(distanceX) > Math.abs(distanceY)) {
+                return true;
+            }
+            return false;
+        }
+    }
+    */
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -111,7 +109,7 @@ public class SlideLayout extends ViewGroup {
         int height = MeasureSpec.getSize(heightMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
         setMeasuredDimension(width, height);
-        View mMenuLayout = getChildAt(0);
+        View mMenuLayout = getChildAt(1);
         Log.d(TAG, "onMeasure: getChildAt(0)");
         MarginLayoutParams menulp = (MarginLayoutParams) mMenuLayout.getLayoutParams();
         int menuWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec
@@ -121,7 +119,7 @@ public class SlideLayout extends ViewGroup {
         menuLayout.measure(menuWidthMeasureSpec, menuHeightMeasureSpec);
 
 
-        View mContentLayout = getChildAt(1);
+        View mContentLayout = getChildAt(0);
         Log.d(TAG, "onMeasure: getChildAt(1)");
         MarginLayoutParams contentlp = (MarginLayoutParams) mContentLayout.getLayoutParams();
 
@@ -137,10 +135,12 @@ public class SlideLayout extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        Log.d(TAG, "onLayout: called");
         View menu = menuLayout;
         View content = contentLayout;
         MarginLayoutParams menulp = (MarginLayoutParams) menu.getLayoutParams();
         int menuLeft = (int) (-menuLayout.getMeasuredWidth() * (1.0f - mOffset));
+        Log.d(TAG, "onLayout: " + menuLeft);
         menu.layout(menuLeft, menulp.topMargin, menuLeft + menu.getMeasuredWidth()
                 , menulp.topMargin + menu.getMeasuredHeight());
 
@@ -152,34 +152,43 @@ public class SlideLayout extends ViewGroup {
 
     @Override
     public void computeScroll() {
+        Log.d(TAG, "computeScroll: called");
         //Log.d(TAG,Log.getStackTraceString(new Throwable()));
-        if (mDragHelper !=null &&mDragHelper.continueSettling(true)) {
+        if (mDragHelper.continueSettling(true)) {
             ViewCompat.postInvalidateOnAnimation(menuLayout);
         }
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        boolean intercept = mDragHelper.shouldInterceptTouchEvent(ev);
-        return intercept;
+        Log.d(TAG, "onInterceptTouchEvent: called");
+        final int action = MotionEventCompat.getActionMasked(ev);
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mDragHelper.cancel();
+            return false;
+        }
+        return mDragHelper.shouldInterceptTouchEvent(ev);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: called");
         mDragHelper.processTouchEvent(event);
         return true;
     }
 
     @Override
     protected void onFinishInflate() {
-        super.onFinishInflate();
+
         Log.d(TAG, "onFinishInflate: getChildAt");
-        menuLayout = getChildAt(0);
-        contentLayout = getChildAt(1);
+        menuLayout = getChildAt(1);
+        contentLayout = getChildAt(0);
+        super.onFinishInflate();
     }
 
     @Override
     protected LayoutParams generateDefaultLayoutParams() {
+
         return new MarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
     }
 
@@ -192,4 +201,7 @@ public class SlideLayout extends ViewGroup {
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
         return new MarginLayoutParams(getContext(),attrs);
     }
+
+    
+
 }
